4cb70b32018d9e8b4a6655e7245beb6c9be651ac
acegene <acegene22@gmail.com>
2022-03-06 06:19:59 +0200

    M write_btw.py re_utils.py: refactor

--- py/utils/re_utils.py
+++ py/utils/re_utils.py
@@ -1,8 +1,8 @@
 import re
 
-from typing import Pattern
+from typing import Callable, Pattern, Sequence, Tuple, Union
 
-# TODO: consider filename length restriction
+#### TODO: consider filename length restriction
 
 #### Regex criteria for filenames; a match typically indicates an erroneous filename
 #### Criteria of the same number are mutually exclusive i.e do not use 1a alongside 1b
@@ -32,4 +32,37 @@ def regex_or_operation(*regexes: str) -> Pattern:
 #### Precompiled regexes
 REGEX_FN_STRICT_1 = regex_or_operation(REG_STR_FN_1A, REG_STR_FN_2A, REG_STR_FN_3A, REG_STR_FN_4A)
 REGEX_FN_STRICT_2 = regex_or_operation(REG_STR_FN_1B, REG_STR_FN_2B, REG_STR_FN_3A, REG_STR_FN_4A)
-REGEX_FN_HAS_UPPER = re.compile(REG_STR_FN_1C)
+REGEX_FN_HAS_UPPER = re.compile(REG_STR_FN_1C)  # TODO: does this need '^(' or ')$'?
+
+ReplaceType = Union[str, Callable[[str], str], None]
+
+
+def re_replace(reg: str, str_in: str, group_replace: Sequence[ReplaceType], indices=None, flags=0) -> Tuple[str, int]:
+    def __create_replace_str(str_in: str, replace: ReplaceType):
+        if replace is None:
+            return str_in
+        if isinstance(replace, str):
+            return replace
+        return replace(str_in)
+
+    if re.compile(reg, flags=flags).groups != len(group_replace):
+        raise ValueError(
+            f"num capture-groups != len(group_replace): {re.compile(reg, flags=flags).groups} != {len(group_replace)}"
+        )
+    str_out = str_in
+    offset = 0
+    selected = re_select(reg, str_in, flags)
+    for j, match in enumerate(selected):
+        if indices is None or j in indices:
+            for i, group in enumerate(match):
+                str_repl = __create_replace_str(group[0], group_replace[i] if group_replace[i] is not None else None)
+                str_out = str_repl.join([str_out[: group[1][0] + offset], str_out[group[1][1] + offset :]])
+                offset += len(str_repl) - (group[1][1] - group[1][0])
+    return str_out, len(selected)
+
+
+def re_select(reg: str, str_in: str, flags=0) -> Sequence[Sequence[Tuple[str, Tuple[int, int]]]]:
+    return [
+        [(group, match.span(i + 1)) for i, group in enumerate(match.groups())]
+        for match in re.finditer(reg, str_in, flags=flags)
+    ]
--- py/write_btw.py
+++ py/write_btw.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 #
-# title: write_btw.py
-#
 # owner: acegene
 #
 # descr: manipulate/handle str between str_beg and str_end for a given str/file/pipe; matches work across lines
@@ -16,129 +14,139 @@
 #              stdout: "zzOUTww"
 #        * write_btw.py -b BEG -e END -o OUT --str "zzBEGyyENDxxBEGwwENDvv" --pattern-btw-type greedy
 #              stdout: "zzOUTvv"
-#        * write_btw.py -b BEG -e END -o OUT --file-stdout FILE_PATH
+#        * write_btw.py -b BEG -e END -o OUT --file FILE_PATH
 #              stdout: "zzOUTxx" # if FILE_PATH content is "zzBEGyyENDxx"
-#        * write_btw.py -b BEG -e END -o OUT --file-write FILE_PATH
+#        * write_btw.py -b BEG -e END -o OUT --file FILE_PATH --file-mode write
 #              if FILE_PATH content is "zzBEGyyENDxx" it will be overwritten by "zzOUTxx"
 #        * printf "zzBEGyyENDxx" | write_btw.py -b BEG -e END -o OUT --pipe
 #              stdout: "zzOUTxx"
 #
-# notes: * compatible with HERE docs if using --pipe: https://stackoverflow.com/a/10677233
-#        * newline type of files is preserved ('\n' vs '\r\n'); platform executing script should be irrelevant
+# notes: * use HERE docs with --pipe: https://stackoverflow.com/a/10677233
+#        * use HERE docs to assign to vars for input cmd args: https://stackoverflow.com/a/1655389
+#        * preserves file newline type ('\n' vs '\r\n'); platform executing script should be irrelevant
 #        * this scripts intent is to be robust rather than fast
+#        * text encodings for python: https://stackoverflow.com/a/25584253
 #
 # warns: * this script has no file locking mechanism
-#           * problems could arise with large files (2-3 GB for 32 bit python and 64GB for 64 bit python)
+#        * problems could arise with large files (2-3 GB for 32 bit python and 64GB for 64 bit python)
 #
 # todos: * option for handling overlapping matches
 #        * user specified capture groups and related potential behavior
 #        * option to match str_beg and end-of-str_in if matching str_end no found; similar for str_end
 #        * handle sourcing and calling from another script with regards to logging
 #        * pattern matching for str_beg and str_end
+#        * add more thorough encoding checks and exception handling
+#        * add option to write to stdout as binary (and maybe read from stdin as binary?)
+#              https://stackoverflow.com/a/27185688
+#              https://stackoverflow.com/a/58664856
+#        * consider auto encoding detection confidence from chardet (or replacing chardet alltogether?)
+#        * carefully consider each usage of newline=""
+#        * consider checking if content matches file before writing
 
 import argparse
 import enum
+import io
 import logging
 import os
 import re
 import sys
 
-from typing import Any, Callable, Dict, Optional, Sequence, Tuple, Union
+from typing import Any, Dict, Optional, NoReturn, Sequence, Tuple, Union
+
+import chardet
+
+from utils import re_utils
 
 PathLike = Union[str, bytes, os.PathLike]
 
-NAME_THIS = os.path.basename(__file__)
+NAME_THIS = "write_btw.py"
 
 
-class MetaEnum(enum.EnumMeta):
-    def __is_exaustive(cls, keys: Sequence[Any]) -> bool:
-        if len(cls) != len(keys):
-            return False
-        for key in keys:
-            if key not in cls:
-                return False
-        return True
-
-    def from_str(cls, label: str) -> "MetaEnum":
-        try:
-            return cls[label]
-        except KeyError:
-            raise ValueError(f"{cls.__name__}.from_str cannot accept input='{label}'")
-
-    CallableTuple = Tuple[Callable]
-    CallableTupleWithArgs = Tuple[Callable, Sequence[Any]]
-    CallableTupleWithArgsWithKWArgs = Tuple[Callable, Sequence[Any], Dict[str, Any]]
-    CallableTupleWithOrWithoutArgs = Union[CallableTuple, CallableTupleWithArgs, CallableTupleWithArgsWithKWArgs]
-
-    def exaustive_actions(cls, enum_state: "MetaEnum", actions: Dict["MetaEnum", CallableTupleWithOrWithoutArgs]):
-        if not cls.__is_exaustive(actions.keys()):
-            raise ValueError(f"{cls.__name__}.exaustive_actions requires all enum states are covered by 'actions' arg")
-        action = actions[enum_state]
-        if len(action) == 1:
-            return action[0]()
-        if len(action) == 2:
-            return action[0](*action[1])
-        if len(action) == 3:
-            return action[0](*action[1], **action[2])
-        raise ValueError(f"{cls.__name__}.exaustive_actions requires len(action) equal to 1-3")
-
-    def exaustive_assigns(cls, enum_state: "MetaEnum", assigns: Dict["MetaEnum", Any]):
-        if not cls.__is_exaustive(assigns.keys()):
-            raise ValueError(f"{cls.__name__}.exaustive_assigns requires all enum states are covered by 'actions' arg")
-        return assigns[enum_state]
+def assert_unreachable(value: NoReturn) -> NoReturn:
+    assert False, f"Unhandled value: {value} ({type(value).__name__})"
 
 
-class BtwMode(enum.Enum, metaclass=MetaEnum):
+class BtwMode(enum.Enum):
     FILE = 0
     STR = 1
 
 
-class BtwPattern(enum.Enum, metaclass=MetaEnum):
+class BtwPattern(enum.Enum):
     NONGREEDY = 0
     GREEDY = 1
 
 
 def setup_logger(logger_name: str) -> logging.Logger:
     #### create logger
-    logger = logging.getLogger(logger_name)
-    logger.setLevel(logging.INFO)
+    logger_out = logging.getLogger(logger_name)
+    logger_out.setLevel(logging.INFO)
     #### create console handler
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)
+    console_hdlr = logging.StreamHandler()
+    console_hdlr.setLevel(logging.INFO)
     #### create formatter
     formatter = logging.Formatter("%(levelname)s: %(name)s: %(message)s")
     #### add formatter to handlers
-    ch.setFormatter(formatter)
+    console_hdlr.setFormatter(formatter)
     #### add handlers to logger
-    logger.addHandler(ch)
-    return logger
+    logger_out.addHandler(console_hdlr)
+    return logger_out
 
 
 def parse_inputs(cmd_args: Optional[Sequence[str]] = None) -> Dict[str, Any]:
     """Parse cmd line inputs; set, check, and fix script's default vars"""
     #### cmd args parser
-    parser = argparse.ArgumentParser(cmd_args, usage=usage())
-    ## top level parser
+    parser = argparse.ArgumentParser(usage=usage())
+    ## str args
     parser.add_argument("--str-beg", "-b", required=True, help="str the found pattern must beg with")
     parser.add_argument("--str-end", "-e", required=True, help="str the found pattern must end with")
-    parser.add_argument("--str-out", "-o", required=True, help="str to replace 'str_beg + pattern + str_end' with")
     parser.add_argument(
-        "--indices", nargs="+", type=check_pos_int, help="handle matches[n] where n is specified indices (0=1st)"
+        "--str-out",
+        "-o",
+        required=True,
+        help="str to replace 'str_beg + pattern + str_end' with",
+    )
+    ## misc args
+    parser.add_argument(
+        "--indices",
+        nargs="+",
+        type=check_pos_int,
+        help="handle matches[n] where n is specified indices (0=1st)",
     )
     parser.add_argument(
-        "--error-no-matches", action="store_true", default=False, help="execute sys.exit(1) if no matches are found"
+        "--error-no-matches",
+        action="store_true",
+        default=False,
+        help="execute sys.exit(1) if no matches are found",
     )
+    ## encoding args
+    # parser.add_argument("--encoding-fallback", help='encoding codec to use when one fails')
+    parser.add_argument("--encoding-file", help="encoding codec to use for file")
+    parser.add_argument(
+        "--encoding-stdin",
+        default=sys.stdin.encoding,
+        help="encoding codec to use for stdin",
+    )
+    parser.add_argument(
+        "--encoding-stdout",
+        default=sys.stdin.encoding,
+        help="encoding codec for stdout",
+    )
+    parser.add_argument("--file-mode", choices=["stdout", "write"], default="stdout", help="")
     ## mutually exclusive pattern group
     parser_group_pattern = parser.add_mutually_exclusive_group()
     parser_group_pattern.add_argument("--pattern-btw", type=check_re_pattern, help="pattern to use; NOT recommended")
     parser_group_pattern.add_argument("--pattern-btw-type", choices=["greedy", "nongreedy"], default="nongreedy")
-    parser_group_mode = parser.add_mutually_exclusive_group()
-    parser_group_mode.add_argument("--file-stdout", type=is_file, help="extract str_in from file; write to stdout")
-    parser_group_mode.add_argument("--file-write", type=is_file, help="extract str_in from file; overwrite file")
+    ## mutually exclusive mode group
+    parser_group_mode = parser.add_mutually_exclusive_group(required=True)
+    parser_group_mode.add_argument("--file", type=check_file, help="extract str_in from file")
     parser_group_mode.add_argument("--str", help="str to use as str_in")
-    parser_group_mode.add_argument("--pipe", action="store_true", help="extract str to use as str_in from pipe/stdin")
+    parser_group_mode.add_argument(
+        "--pipe",
+        action="store_true",
+        help="extract str to use as str_in from pipe/stdin",
+    )
     ## parse cmd args
-    args = parser.parse_args()
+    args = parser.parse_args(cmd_args)
     #### create output dictionary
     args_parsed = {}
     #### fill output dictionary
@@ -148,26 +156,42 @@ def parse_inputs(cmd_args: Optional[Sequence[str]] = None) -> Dict[str, Any]:
     args_parsed["str_out"] = args.str_out
     args_parsed["indices"] = args.indices
     args_parsed["error_no_matches"] = args.error_no_matches
+    args_parsed["encoding_stdout"] = args.encoding_stdout
     ## fill output pattern_btw
-    if args.pattern_btw != None:
+    if args.pattern_btw is not None:
         args_parsed["pattern_btw"] = args.pattern_btw
     else:
-        args_parsed["pattern_btw"] = BtwPattern.exaustive_assigns(
-            BtwPattern.from_str(args.pattern_btw_type.upper()), {BtwPattern.NONGREEDY: r".*?", BtwPattern.GREEDY: r".*"}
-        )
+        pattern_type = BtwPattern[args.pattern_btw_type.upper()]
+        if pattern_type is BtwPattern.GREEDY:
+            args_parsed["pattern_btw"] = r"(.*)"
+        elif pattern_type is BtwPattern.NONGREEDY:
+            args_parsed["pattern_btw"] = r"(.*?)"
+        else:
+            assert_unreachable(args_parsed["pattern_btw"])
     ## fill mode specific args
-    if args.file_stdout != None or args.file_write != None:
+    if args.file is not None:
         args_parsed["mode"] = BtwMode.FILE
-        args_parsed["file"] = args.file_stdout if args.file_stdout != None else args.file_write
-        args_parsed["file_mode"] = "stdout" if args.file_stdout != None else "write"
-    elif args.str != None:
+        args_parsed["file"] = args.file
+        args_parsed["file_mode"] = args.file_mode
+        if args.encoding_file is not None:
+            args_parsed["encoding_file"] = args.encoding_file
+        else:
+            with open(__file__, "rb") as f:
+                args_parsed["encoding_file"] = chardet.detect(f.read())["encoding"]
+                logger.info(
+                    "for file='%s' using auto detected encodings='%s'.",
+                    args_parsed["file"],
+                    args_parsed["encoding_file"],
+                )
+    elif args.str is not None:
         args_parsed["mode"] = BtwMode.STR
         args_parsed["str_in"] = args.str
-    elif args.pipe == True:
+    elif args.pipe is True:
         args_parsed["mode"] = BtwMode.STR
-        args_parsed["str_in"] = sys.stdin.read()
+        stdin_encoded = io.TextIOWrapper(sys.stdin.buffer.raw, encoding=args.encoding_stdin)  # type: ignore[attr-defined] # pylint: disable=line-too-long
+        args_parsed["str_in"] = stdin_encoded.read()
     else:
-        logger.error(f"could not find any possible value for mode")
+        logger.error("could not find any possible value for mode")
         sys.exit(1)
 
     return args_parsed
@@ -175,25 +199,31 @@ def parse_inputs(cmd_args: Optional[Sequence[str]] = None) -> Dict[str, Any]:
 
 def usage() -> str:
     usage_str = ""
-    with open(__file__, "r") as f:
-        for line in f.readlines():
-            if not line.startswith("#"):
-                return usage_str
-            usage_str += line
+    try:
+        with open(__file__, "r", encoding="utf-8") as f:
+            for line in f.readlines():
+                if not line.startswith("#"):
+                    return usage_str
+                usage_str += line
+    except OSError as e:
+        if e.errno == 2:
+            usage_str = "WARNING: could not generate usage."
+        else:
+            raise OSError from e
     return usage_str
 
 
-def is_file(value: Any) -> PathLike:
+def check_file(value: Any) -> PathLike:
     if not os.path.exists(value):
         raise argparse.ArgumentTypeError(f"{value} is an invalid file path")
-    return value
+    return value  # type: ignore[no-any-return]
 
 
 def check_pos_int(value: Any) -> int:
     try:
         int_from_value = int(value)
-    except (TypeError, ValueError) as e:
-        raise argparse.ArgumentTypeError(f"{value} is an invalid positive int value")
+    except (TypeError, ValueError):
+        raise argparse.ArgumentTypeError(f"{value} is an invalid positive int value") from None
     if int_from_value < 0:
         raise argparse.ArgumentTypeError(f"{value} is an invalid positive int value")
     return int_from_value
@@ -203,28 +233,20 @@ def check_re_pattern(value: str) -> str:
     try:
         re.compile(value)
     except re.error:
-        raise argparse.ArgumentTypeError(f"{value} is an invalid regex pattern")
+        raise argparse.ArgumentTypeError(f"{value} is an invalid regex pattern") from None
     return value
 
 
-def str_replace_range(str_in: str, index_start: int, index_end: int, str_replace: str) -> str:
-    return str_in[0:index_start] + str_replace + str_in[index_end:]
-
-
 def str_btw_internal(
-    str_in: str, str_beg: str, str_end: str, str_out: str, pattern_btw: str, indices: Optional[Sequence[int]]
-) -> Tuple[int, str]:
+    str_in: str,
+    str_beg: str,
+    str_end: str,
+    str_out: str,
+    pattern_btw: str,
+    indices: Optional[Sequence[int]],
+) -> Tuple[str, int]:
     pattern = re.escape(str_beg) + pattern_btw + re.escape(str_end)
-    matches = re.finditer(pattern, str_in, flags=re.S)
-    str_final = str_in
-    offset = 0
-    num_matches = 0
-    for i, match in enumerate(matches):
-        num_matches += 1
-        if indices == None or i in indices:
-            str_final = str_replace_range(str_final, match.start() + offset, match.end() + offset, str_out)
-            offset += len(str_out) - (match.end() - match.start())
-    return str_final, num_matches
+    return re_utils.re_replace(pattern, str_in, group_replace=[str_out], indices=indices, flags=re.S)
 
 
 def file_btw(
@@ -235,32 +257,38 @@ def file_btw(
     str_out: str,
     pattern_btw: str,
     indices: Optional[Sequence[int]],
+    encoding_file: str,
+    encoding_stdout: str,
 ) -> int:
-    with open(file_, "r", newline="") as f:
+    with open(file_, "r", newline="", encoding=encoding_file) as f:
         str_in = f.read()
     str_final, num_matches = str_btw_internal(str_in, str_beg, str_end, str_out, pattern_btw, indices)
     if file_mode == "stdout":
-        print(str_final, end="")
+        io.TextIOWrapper(sys.stdout.buffer.raw, newline="", encoding=encoding_stdout).write(str_final)  # type: ignore[attr-defined] # pylint: disable=line-too-long
     elif file_mode == "write":
         #### prevent unnecessary overwriting of file if no matches were found
         if num_matches != 0:
-            with open(file_, "w") as f:
+            with open(file_, "w", newline="", encoding=encoding_file) as f:
                 f.seek(0)
                 f.write(str_final)
                 f.truncate()
     else:
-        for symbol, value in locals().items():
-            print((symbol, value))
-        logger.error(f"unexpected file_mode='{file_mode}'")
+        logger.error("unexpected file_mode='%s'", file_mode)
         sys.exit(1)
     return num_matches
 
 
 def str_btw(
-    str_in: str, str_beg: str, str_end: str, str_out: str, pattern_btw: str, indices: Optional[Sequence[int]]
+    str_in: str,
+    str_beg: str,
+    str_end: str,
+    str_out: str,
+    pattern_btw: str,
+    indices: Optional[Sequence[int]],
+    encoding_stdout: str,
 ) -> int:
     str_final, num_matches = str_btw_internal(str_in, str_beg, str_end, str_out, pattern_btw, indices)
-    print(str_final, end="")
+    io.TextIOWrapper(sys.stdout.buffer.raw, encoding=encoding_stdout).write(str_final)  # type: ignore[attr-defined]
     return num_matches
 
 
@@ -268,35 +296,32 @@ def write_btw(cmd_args: Optional[Sequence[str]] = None) -> None:
     #### parse and check cmd line args
     args_parsed = parse_inputs(cmd_args)
     #### select and call func with appropriate args depending on mode
-    num_matches = BtwMode.exaustive_actions(
-        args_parsed["mode"],
-        {
-            BtwMode.FILE: (
-                file_btw,
-                (
-                    args_parsed.get("file", None),
-                    args_parsed.get("file_mode", None),
-                    args_parsed.get("str_beg", None),
-                    args_parsed.get("str_end", None),
-                    args_parsed.get("str_out", None),
-                    args_parsed.get("pattern_btw", None),
-                    args_parsed.get("indices", None),
-                ),
-            ),
-            BtwMode.STR: (
-                str_btw,
-                (
-                    args_parsed.get("str_in", None),
-                    args_parsed.get("str_beg", None),
-                    args_parsed.get("str_end", None),
-                    args_parsed.get("str_out", None),
-                    args_parsed.get("pattern_btw", None),
-                    args_parsed.get("indices", None),
-                ),
-            ),
-        },
-    )
-    sys.exit(1 if args_parsed["error_no_matches"] and num_matches == 0 else 0)
+    if args_parsed["mode"] is BtwMode.FILE:
+        num_matches = file_btw(
+            args_parsed["file"],
+            args_parsed["file_mode"],
+            args_parsed["str_beg"],
+            args_parsed["str_end"],
+            args_parsed["str_out"],
+            args_parsed["pattern_btw"],
+            args_parsed["indices"],
+            args_parsed["encoding_file"],
+            args_parsed["encoding_stdout"],
+        )
+    elif args_parsed["mode"] is BtwMode.STR:
+        num_matches = str_btw(
+            args_parsed["str_in"],
+            args_parsed["str_beg"],
+            args_parsed["str_end"],
+            args_parsed["str_out"],
+            args_parsed["pattern_btw"],
+            args_parsed["indices"],
+            args_parsed["encoding_stdout"],
+        )
+    else:
+        assert_unreachable(args_parsed["mode"])
+    if args_parsed["error_no_matches"] and num_matches == 0:
+        sys.exit(1)
 
 
 if __name__ == "__main__":
