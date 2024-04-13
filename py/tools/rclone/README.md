260caafbe6ff33ff817adcc487389ca0ec3e58ff
acegene <acegene22@gmail.com>
2024-04-13 09:57:36 -0400

    M rclone_bisync: Add --init

--- py/tools/rclone/README.md
+++ py/tools/rclone/README.md
@@ -1,28 +1,12 @@
 ## Installation
 https://rclone.org/install/
-- windows
-    - download exe and place in ~/bin 
 - linux
-    - sudo -v ; curl https://rclone.org/install.sh | sudo bash
+    - `sudo -v ; curl https://rclone.org/install.sh | sudo bash`
+- windows
+    - download exe and place in ~/.local/bin
 
-## Config setup
-One time setup configs:
-- Google Drive:
-    - setup non encrypted acccess
-        - https://rclone.org/drive/
-            - `rclone config`
-                - name=remote
-    - setup encrypted access
-        - https://rclone.org/crypt/
-            - `rclone config`
-                - name=secret
-                - remote=remote:crypt
-
-- location of cfg:
-    - linux: ~/.config/rclone/rclone.conf
-    - windows: ~\AppData\Roaming\rclone\rclone.conf
-
-## One time setup of remote:
+## Remote setup:
+### One time setup
 ```
 ## need to manually create synced/crypt synced/uncrypt dirs on remote
 rclone mkdir remote:synced/uncrypt/bk
@@ -32,16 +16,26 @@ rclone mkdir secret:bk
 rclone touch secret:content/text/RCLONE_TEST
 ```
 
-## One time setup of local powershell:
-```
-## need to manually create "$($HOME)\Documents\synced\crypt\text\" "$($HOME)\Documents\synced\uncrypt\text\"
-$loc_unc="$($HOME)\Documents\synced\uncrypt\text"
-$rem_unc='remote:synced/uncrypt/content/text'
-rclone copyto "$($rem_unc)/RCLONE_TEST" "$($loc_unc)\RCLONE_TEST"
-rclone bisync --resync "$($loc_unc)" "$($rem_unc)" --check-access --verbose
+## Local setup:
+### One time rclone config setup:
+- Google Drive:
+    - setup non encrypted acccess
+        - https://rclone.org/drive/
+            - `rclone config`
+                - name=remote
+    - setup encrypted access
+        - https://rclone.org/crypt/
+            - `rclone config`
+                - name=secret
+                - remote=remote:synced/crypt
 
-$loc_unc="$($HOME)\Documents\synced\crypt\text"
-$rem_unc='secret:content/text'
-rclone copyto "$($rem_unc)/RCLONE_TEST" "$($loc_unc)\RCLONE_TEST"
-rclone bisync --resync "$($loc_unc)" "$($rem_unc)" --check-access --verbose
+- location of cfg:
+    - linux: ~/.config/rclone/rclone.conf
+    - windows: ~\AppData\Roaming\rclone\rclone.conf
+
+### One time setup of local bisync directories:
+```
+rclone copy secret:content/text/cfg/cfg-gws.json .
+rclone_bisync --json-cfg cfg-gws.json --init
+rm cfg-gws.json
 ```
--- py/tools/rclone/rclone_bisync.py
+++ py/tools/rclone/rclone_bisync.py
@@ -5,16 +5,75 @@ import datetime
 import json
 import os
 import subprocess
+import sys
 
 from typing import Dict, List, Optional
 
 from utils import json_utils
 
+## TODO:
+## - cannot specify force via prompt or per bisync execution
+
 
 _JSON_CFG_DEFAULT = os.path.join(os.path.expanduser("~"), ".config", "cfg-gws.json")
+_RCLONE_TEST = "RCLONE_TEST"
 
 
-def run_rclone_bisync(rclone, rclone_bk_details: Dict, force: bool) -> int:
+def find_cfg_file(dir_: str) -> Optional[str]:
+    for root, _, files in os.walk(dir_):
+        for file in files:
+            if file == "cfg-gws.json":
+                return os.path.abspath(os.path.join(root, file))
+    return None
+
+def _init_rclone_bisync(rclone, rclone_bk_details: Dict, json_cfg: str) -> int:
+    found_cfg = _JSON_CFG_DEFAULT if os.path.exists(_JSON_CFG_DEFAULT) else None
+    for rclone_bk_detail in rclone_bk_details:
+        local = json_utils.create_path_from_json_relative_path(rclone_bk_detail["loc"])
+        bk_local = json_utils.create_path_from_json_relative_path(rclone_bk_detail["bk_loc"])
+        remote = os.path.expandvars(rclone_bk_detail["rem"])
+
+        if not os.path.exists(local):
+            os.makedirs(local, exist_ok=True)
+        if not os.path.exists(bk_local):
+            os.makedirs(bk_local, exist_ok=True)
+
+        rclone_test_path = os.path.join(local, _RCLONE_TEST)
+        if not os.path.exists(rclone_test_path):
+            if len(os.listdir(local)) != 0:
+                print(f"ERROR: expected local={local} to be empty")
+                sys.exit(1)
+            rclone_test_cp_params = ["copyto", f"{remote}/RCLONE_TEST", rclone_test_path]
+            rclone_test_cp_params_str = "' '".join(rclone_test_cp_params)
+            cmd_str = f"'{rclone}' '{rclone_test_cp_params_str}'"
+            print(f"INFO: EXEC: {cmd_str}")
+            rclone_result = subprocess.run([rclone] + rclone_test_cp_params, check=False, capture_output=False)
+            if rclone_result.returncode != 0:
+                print(f"ERROR: {cmd_str}; rclone_result.returncode={rclone_result.returncode}")
+                return rclone_result.returncode
+
+            resync_params = ["bisync", "--resync", local, remote, "--check-access", "--verbose"]
+            resync_params_str = "' '".join(resync_params)
+            cmd_str = f"'{rclone}' '{resync_params_str}'"
+            resync_result = subprocess.run([rclone] + resync_params, check=False, capture_output=False)
+            if resync_result.returncode != 0:
+                print(f"ERROR: {cmd_str}; resync_result.returncode={resync_result.returncode}")
+                return resync_result.returncode
+        if found_cfg is None:
+            found_cfg = find_cfg_file(local)
+
+    if not os.path.exists(_JSON_CFG_DEFAULT):
+        assert found_cfg is not None
+        parent_dir = os.path.dirname(_JSON_CFG_DEFAULT)
+        os.makedirs(parent_dir, exist_ok=True)
+        found_cfg_abs = os.path.abspath(found_cfg)
+        os.symlink(found_cfg_abs, _JSON_CFG_DEFAULT)
+        print(f"INFO: created symlink '{found_cfg_abs}' -> '{_JSON_CFG_DEFAULT}'")
+
+    return 0
+
+
+def _run_rclone_bisync(rclone, rclone_bk_details: Dict, force: bool) -> int:
     for rclone_bk_detail in rclone_bk_details:
         local = json_utils.create_path_from_json_relative_path(rclone_bk_detail["loc"])
         bk_local = json_utils.create_path_from_json_relative_path(rclone_bk_detail["bk_loc"])
@@ -49,17 +108,19 @@ def run_rclone_bisync(rclone, rclone_bk_details: Dict, force: bool) -> int:
         if force:
             bisync_params.append("--force")
 
-        quoted_space = "' '"
-        print(f"INFO: EXEC: '{rclone}' '{quoted_space.join(bisync_params)}'")
+        bisync_params_str = "' '".join(bisync_params)
+        cmd_str = f"'{rclone}' '{bisync_params_str}'"
+        print(f"INFO: EXEC: {cmd_str}")
         bisync_result = subprocess.run([rclone] + bisync_params, check=False, capture_output=False)
         if bisync_result.returncode != 0:
-            print(f"ERROR: '{rclone}' '{quoted_space.join(bisync_params)}'; bisync_result.returncode={bisync_result.returncode}")
+            print(f"ERROR: {cmd_str}; bisync_result.returncode={bisync_result.returncode}")
             return bisync_result.returncode
     return 0
 
 
 def main(argparse_args: Optional[List[str]] = None) -> int:
     parser = argparse.ArgumentParser(description="Run rclone bisync with details extracted from json.")
+    parser.add_argument("--init", action="store_true", help="TODO")
     parser.add_argument("--force", action="store_true", help="Run in situations like too many deletes.")
     parser.add_argument("--json-cfg", default=_JSON_CFG_DEFAULT, help="Json cfg file to extract bisync backup settings")
     parser.add_argument("--rclone", default="rclone", help="The rclone version to use")
@@ -68,8 +129,11 @@ def main(argparse_args: Optional[List[str]] = None) -> int:
     with open(args.json_cfg, "r", encoding="utf-8") as file:
         rclone_bk_details = json.load(file)["rclone_bk_details"]
 
-    return run_rclone_bisync(args.rclone, rclone_bk_details, args.force)
+    if args.init:
+        return _init_rclone_bisync(args.rclone, rclone_bk_details, args.json_cfg)
+
+    return _run_rclone_bisync(args.rclone, rclone_bk_details, args.force)
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
